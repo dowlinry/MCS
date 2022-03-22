167dbf6b7197f0b77ca1a8c0cb78795d8e4f7f17
dowlinry <37183848+dowlinry@users.noreply.github.com>
2022-03-22 23:23:39 +0000

    api updates

--- ProductivityTracker/productivity-viz/src/app/api-service/api-service.service.ts
+++ ProductivityTracker/productivity-viz/src/app/api-service/api-service.service.ts
@@ -10,13 +10,14 @@ import config from 'assets/config';
 })
 export class ApiServiceService {
 
-
+  
   constructor(private http: HttpClient) { }
 
   private firebaseURL: any = config.firebaseDatabaseURL;
   private accessToken: any = config.githubAccessToken;
   private githubUsername: any = config.githubUsername;
   private repos: any = config.repos;
+  private commits: any = []
 
   private octokit = new Octokit({
     auth: this.accessToken
@@ -34,14 +35,79 @@ export class ApiServiceService {
     this.githubUsername = name;
   }
 
-  public getCommitData(){
-    this.repos.forEach((repo: any) => {
-      this.octokit.rest.repos.listCommits({
-        owner: this.githubUsername,
-        repo: repo.name,
-      }).then(console.log)
+  public async getCommitData(){
+    this.repos.forEach(async (repo: any) => {
+      const repoCommits = await this.getRepoCommits(repo);
+      
+      for(const commit of repoCommits){
+        const details = await this.getCommitDetails(commit);
+        console.log(details);
+      }
+    })
+    
+  }
+
+  private async getRepoCommits(repo: any){
+      let repoCommits: any  = [];
+      const branches: any = await this.getBranches(repo);
+      
+      branches.data.forEach(async (branch: any) => {
+        const commits: any = await this.getBranchCommits(repo, branch)
+        
+        commits.data.forEach((commit: any) => {
+          if(this.isUserCommit(commit)){
+            repoCommits[commit.commit.author.date] = commit.commit;
+          }
+        })
+      })
+
+      return await repoCommits;
+  }
+  private async getBranches(repo: any){
+    return await this.octokit.rest.repos.listBranches({
+      owner: this.githubUsername,
+      repo: repo.name,
     })
   }
+
+  private async getBranchCommits(repo: any, branch: any){
+    return await this.octokit.rest.repos.listCommits({
+      owner: this.githubUsername,
+      repo: repo.name,
+      sha: branch.name
+    })
+  }
+
+  private isUserCommit(commit: any){
+    if(commit.author.login === this.githubUsername){
+      return true;
+    } else return false;
+  }
+
+  private sortRepoCommits(commits: any, repo: any){
+    const commitTimestamps = Object.keys(commits).sort();
+    commitTimestamps.forEach((commit: any) => {
+      console.log(commit)
+    })
+  }
+
+  private async getCommitDetails(commit: any){
+    return await commit;
+  }
+
+  private async test(x: any){
+    console.log(x);
+  }
+  // private async test(repo: any){
+  //   this.http.get(
+  //     `https://api.github.com/repos/${this.githubUsername}${repo.name}/branches`,
+  //     {
+  //       headers:{
+  //         authorization: this.accessToken
+  //       }
+  //     }
+  //   ).subscribe(console.log)
+  // }
 
   public getFirebaseData(){}
 }
